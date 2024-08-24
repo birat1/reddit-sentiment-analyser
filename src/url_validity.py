5d376dd9e68d0fc9cedc7017f4202e5383af8928
birat <93601583+birat1@users.noreply.github.com>
2024-08-25 00:08:47 +0100

    Implemented Reddit URL Checker

---
+++ src/url_validity.py
@@ -0,0 +1,21 @@
+from urllib.parse import urlparse
+
+    
+def is_valid_reddit_url(url: str) -> bool:
+    try:
+        parsed_url =  urlparse(url)
+        # Check if the URL is on 'reddit.com' domain
+        if parsed_url.netloc not in ['www.reddit.com', 'old.reddit.com']:
+            return False
+        
+        # Split the URL into parts
+        path_parts = parsed_url.path.strip('/').split('/')
+        
+        # Validate URL for Reddit post
+        return (
+            (len(path_parts) >= 3 and path_parts[0] == 'r' and path_parts[2] == 'comments') or
+            (len(path_parts) >= 2 and path_parts[0] == 'comments')
+        )
+    except Exception as e:
+        print(f"Error validating URL: {e}")
+        return False
