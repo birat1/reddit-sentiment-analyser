94e9d1b62d2c1aeb85e33d80261ac470a91093df
birat <93601583+birat1@users.noreply.github.com>
2024-09-04 23:41:44 +0100

    Improved formatting

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,25 +1,25 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('analyzeForm');
-    const input = document.getElementById('url');
-    const clearButton = document.getElementById('clear-button');
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("analyzeForm");
+  const input = document.getElementById("url");
+  const clearButton = document.getElementById("clear-button");
 
-    form.addEventListener('submit', (event) => {
-        if (!input.value) {
-            event.preventDefault();
-        }
-    });
+  form.addEventListener("submit", (event) => {
+    if (!input.value) {
+      event.preventDefault();
+    }
+  });
 
-    input.addEventListener('input', function() {
-        if (input.value.length > 0) {
-            clearButton.style.display = 'block';
-        } else {
-            clearButton.style.display = 'none';
-        }
-    });
+  input.addEventListener("input", function () {
+    if (input.value.length > 0) {
+      clearButton.style.display = "block";
+    } else {
+      clearButton.style.display = "none";
+    }
+  });
 
-    clearButton.addEventListener('click', function() {
-        input.value = '';
-        clearButton.style.display = 'none';
-        input.blur();
-    })
+  clearButton.addEventListener("click", function () {
+    input.value = "";
+    clearButton.style.display = "none";
+    input.blur();
+  });
 });
