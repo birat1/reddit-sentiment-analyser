dc5e9e535d75227e4e9e6c08b2e12ffe02e6721b
ba01081 <93601583+birat1@users.noreply.github.com>
2024-09-21 17:09:01 +0100

    Disabled SSL Check

--- src/preprocess_data.py
+++ src/preprocess_data.py
@@ -3,6 +3,14 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
 
 # Download the necessary NLTK data files
 nltk.download('stopwords')
